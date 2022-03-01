bfdd7ba6351238545db5333f35eb148dc283f96c
Idlidev <idliyout@gmail.com>
2022-03-01 15:30:51 +0530

    [update] book.js -> search.js and set status when online

---
+++ SlashCommands/books/search.js
@@ -0,0 +1,42 @@
+const Discord = require("discord.js");
+const axios = require("axios");
+module.exports = {
+	name: "search",
+	description: "Searches for a given book.",
+	options: [
+		{
+			name: "book",
+			type: "STRING",
+			description: "Provide a book.",
+			required: true,
+		},
+	],
+	run: async (client, interaction, args) => {
+		const book = interaction.options.get("book").value;
+		const bookInfo = await axios
+			.get(`https://www.googleapis.com/books/v1/volumes?q=${book}`)
+			.catch(function (error) {
+				if (error.response) {
+					return interaction.followUp({ content: "No books found." });
+				} else if (error.request) {
+					return interaction.followUp({ content: "No books found." });
+				} else {
+					return interaction.followUp({ content: "No books found." });
+				}
+				return interaction.followUp({ content: "No books found." });
+			});
+		console.log(bookInfo.data.totalItems);
+		if (bookInfo.data.totalItems === 0) {
+			return interaction.followUp({ content: "No books found." });
+		}
+		const bookEmbed = new Discord.MessageEmbed()
+			.setTitle(bookInfo.data.items[0].volumeInfo.title)
+			.setDescription(bookInfo.data.items[0].volumeInfo.description)
+			.setColor("RANDOM")
+			.setThumbnail(
+				bookInfo.data.items[0].volumeInfo.imageLinks.thumbnail
+			);
+
+		interaction.followUp({ embeds: [bookEmbed] });
+	},
+};
--- events/ready.js
+++ events/ready.js
@@ -1,5 +1,6 @@
 const client = require("../index");
 
-client.on("ready", () =>
-    console.log(`${client.user.tag} is up and ready to go!`)
-);
+client.on("ready", () => {
+	console.log(`${client.user.tag} is up and ready to go!`);
+	client.user.setActivity("people read books!", { type: "WATCHING" });
+});
