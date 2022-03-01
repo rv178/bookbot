a32077a7302e4af682228a219ad2791c59bd6d91
Idlidev <idliyout@gmail.com>
2022-03-01 14:54:43 +0530

    [init] slash handler

---
+++ SlashCommands/books/book.js
@@ -0,0 +1,9 @@
+const Discord = require('discord.js');
+
+module.exports = {
+  name: 'book',
+  description: 'U',
+  run: async(client, interaction, args) => {
+     
+  }
+}
---
+++ index.js
@@ -0,0 +1,10 @@
+const { Client, Collection } = require("discord.js");
+const dotenv = require("dotenv").config()
+const client = new Client({
+    intents: 32767,
+});
+module.exports = client;
+client.slashCommands = new Collection();
+
+require("./handler")(client);
+client.login(process.env.TOKEN);
