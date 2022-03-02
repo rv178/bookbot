fe434db186180239b41bec8ff21ebd349fb46d96
Idlidev <idliyout@gmail.com>
2022-03-02 16:10:05 +0530

    [update] help menu

--- src/SlashCommands/info/help.js
+++ src/SlashCommands/info/help.js
@@ -1,43 +1,25 @@
-const Discord = require('discord.js');
-const { readdirSync } = require("fs");
+const Discord = require(`discord.js`);
+const { MessageActionRow, MessageSelectMenu } = require("discord.js");
 
 module.exports = {
-    name: 'help',
-    description: 'This command will show you the list of commands.',
-      options: [{
-        name: "category",
-        type: "STRING",
-        description: "Enter a command category."
-    }],
-    run: async (client, interaction, args) => {
-        const cat = readdirSync("./SlashCommands/")
-        if(!args[0]) {
-            const errEmbed = new Discord.MessageEmbed()
-            .setTitle("Error")
-            .setDescription(`Invalid command category, run \`/help\` without parameter to see the list of category.`)
-            .setColor("RED")
-            return interaction.followUp({ embeds: [errEmbed] })
-       }
-        const input_cat = interaction.options._hoistedOptions[0].value.toLowerCase()
-        if (input_cat) {
-            if(cat.includes(input_cat)) {
-                let cmd_list = readdirSync(`./SlashCommands/${input_cat}`)
-                for(x = 0; x < cmd_list.length; x++) cmd_list[x] = cmd_list[x].split("").slice(0, -3).join("")
-                const embed = new Discord.MessageEmbed()
-                .setTitle(`Command list: ${input_cat}`)
-                .setDescription(`${cmd_list.join(", ")}`)
-                .setColor("RANDOM")
-                
-                interaction.followUp({ embeds: [embed] })
-            } else{
-                const errEmbed = new Discord.MessageEmbed()
-                .setTitle("Error")
-                .setDescription(`Invalid command category, run \`/help\` without parameter to see the list of category.`)
-                .setColor("RED")
-                return interaction.followUp({ embeds: [errEmbed] })
-            }
+	name: "help",
+	description: "BookBot help menu.",
+	run: async (client, interaction, args) => {
+		const embed = new Discord.MessageEmbed()
+			.setColor("BLUE")
+			.setTimestamp()
+			.setFooter({
+				text: interaction.user.tag,
+				iconURL: interaction.user.avatarURL({ dynamic: true }),
+			})
+			.setTitle("BookBot help menu.")
+			.setDescription("Categories:")
+			.addField("Info", "`help`, `ping`")
+			.addField("Books", "`search`");
 
-        }
-
-        }  
-}
+		await interaction.editReply({
+			embeds: [embed],
+			ephemeral: false,
+		});
+	},
+};
