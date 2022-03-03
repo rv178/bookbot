7831ab776b97a058ce94e7ccd1506519f95ff051
Maks <max.centeno64@gmail.com>
2022-03-03 21:35:23 +1100

    Help.js but better

--- src/commands/info/help.js
+++ src/commands/info/help.js
@@ -1,25 +1,52 @@
-const Discord = require(`discord.js`);
-const { MessageActionRow, MessageSelectMenu } = require("discord.js");
+const Discord = require("discord.js");
+const { readdirSync } = require("fs");
 
 module.exports = {
 	name: "help",
-	description: "BookBot help menu.",
+	description: "Show All Commands",
 	run: async (client, interaction, args) => {
-		const embed = new Discord.MessageEmbed()
-			.setColor("BLUE")
-			.setTimestamp()
-			.setFooter({
-				text: interaction.user.tag,
-				iconURL: interaction.user.avatarURL({ dynamic: true }),
-			})
-			.setTitle("BookBot help menu.")
-			.setDescription("Categories:")
-			.addField("Info", "`help`, `ping`")
-			.addField("Books", "`search`, `list`");
+		let categories = [];
 
-		await interaction.editReply({
-			embeds: [embed],
-			ephemeral: false,
+		readdirSync("./src/commands").forEach((dir) => {
+			const commands = readdirSync(`./src/commands/${dir}/`).filter((file) =>
+				file.endsWith(".js")
+			);
+
+			const cmds = commands.map((command) => {
+				let file = require(`../../commands/${dir}/${command}`);
+
+				if (!file.name) return "No command name.";
+
+				let name = file.name.replace(".js", "");
+				let description = file.description;
+
+				return `\`${name}\` : ${description} \n`;
+			});
+
+			let data = new Object();
+
+			data = {
+				name: dir,
+				value: cmds.length === 0 ? "In progress." : cmds.join(" "),
+			};
+
+			categories.push(data);
 		});
-	},
+		const row = new Discord.MessageActionRow()
+			.addComponents(
+				new Discord.MessageButton()
+					.setLabel('Support Server')
+					.setStyle('LINK')
+					.setURL('https://discord.gg/Kk4tSmQXUb')
+					.setEmoji("👀")
+			);
+		const embed = new Discord.MessageEmbed()
+			.setTitle("Here are all of my commands:")
+			.addFields(categories)
+			.setDescription(`Use \`/help\`.`)
+			.setTimestamp()
+			.setColor("BLUE");
+		return interaction.followUp({ embeds: [embed], components: [row] });
+	}
+
 };
