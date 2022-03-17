b8398a88d5ffeda401fb9222bba990e8eef771af
Idlidev <idliyout@gmail.com>
2022-03-17 18:38:40 +0530

    [rm] api

--- src/index.js
+++ src/index.js
@@ -8,22 +8,3 @@ client.slashCommands = new Collection();
 
 require("./utils")(client);
 client.login(process.env.TOKEN);
-
-// express api temporarily commented out
-
-/*
-const express = require("express");
-const app = express();
-require("./api/routes")(app);
-
-app.get("/", function (req, res) {
-	res.send("BookBot API");
-});
-app.get("*", function (req, res) {
-	res.status(404).send("404 Moment");
-});
-
-app.listen(3000, () => {
-	console.log("[!] API Server is up http://localhost:3000");
-});
-*/
