8f3fa59cb4d4669cb5ddbe219f4e7972c946f2eb
Maks <max.centeno64@gmail.com>
2022-07-05 15:37:24 +1000

    [Update] fuck off discord-modals

--- src/index.js
+++ src/index.js
@@ -3,8 +3,6 @@ require("dotenv").config();
 const client = new Client({
 	intents: 32767,
 });
-const discordModals = require("discord-modals");
-discordModals(client);
 module.exports = client;
 client.slashCommands = new Collection();
 require("./utils")(client);
