9ecad8562067add8d991eacc4ac6a107f4055c91
Maks <max.centeno64@gmail.com>
2022-08-14 10:59:16 +1000

    fixed??

--- src/events/guildAdd.js
+++ src/events/guildAdd.js
@@ -4,14 +4,7 @@ const { sendHook } = ("../utils/functions");
 const log = require("../utils/logger");
 
 client.on("guildCreate", async (guild) => {
-	var guild_log;
-	if (process.env.GUILDLOGS) {
-		guild_log = process.env.GUILDLOGS;
-	} else {
-		log.error("guild logs webhook url not given!");
-		process.exit();
-	}
-
+	if(process.env.GUILDLOGS === undefined) return;
 	const webhook = new Discord.WebhookClient({url: process.env.GUILDLOGS});
 	const embed = new Discord.MessageEmbed()
 	.setAuthor({name: `Added to ${guild.name}`, iconURL: guild.iconURL({dynamic:true})})
--- src/events/guildRemove.js
+++ src/events/guildRemove.js
@@ -3,13 +3,8 @@ const client = require("../index");
 const log = require("../utils/logger");
 
 client.on("guildDelete", async (guild) => {
-	var guild_log;
-	if (process.env.GUILDLOGS) {
-		guild_log = process.env.GUILDLOGS;
-	} else {
-		log.error("guild logs webhook url not given!");
-		process.exit();
-	}
+	if(process.env.GUILDLOGS === undefined) return;
+
 	const webhook = new Discord.WebhookClient({url: process.env.GUILDLOGS});
 	const embed = new Discord.MessageEmbed()
 	.setAuthor({name: `Removed from ${guild.name}`, iconURL: guild.iconURL({dynamic:true})})
