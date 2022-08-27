f45b8d7d78fa63967e81f235990c0b5b9aa4c91b
Maks <max.centeno64@gmail.com>
2022-08-27 19:59:21 +1000

    [Add] Funny statuses 🤣

--- src/events/ready.js
+++ src/events/ready.js
@@ -3,5 +3,9 @@ const log = require("../utils/logger");
 
 client.on("ready", () => {
 	log.warn(`Logged in as ${client.user.tag}.`);
-	client.user.setActivity("people read books!", { type: "WATCHING" });
+	setInterval(async ()=>{
+        let textList = ['Books are for nerds','Reading a book!',`${client.user.guilds.size} servers!`, 'Books', 'Books are cool', 'imagine a book']
+        var text = textList[Math.floor(Math.random() * textList.length)];
+        client.user.setActivity(text , { type: 'WATCHING' })
+    },30000) // milliseconds
 });
